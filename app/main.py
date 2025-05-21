31141443a9160ccd5d973166eef09de035aac9e7
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-05-21 19:57:55 +0300

    Added asyncio, implemented parallel working

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import time
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
+import asyncio
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
@@ -13,32 +14,50 @@ def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
 
+    hue_light_id = asyncio.create_task(
+        service.register_device(hue_light)
+    )
+    speaker_id = asyncio.create_task(
+        service.register_device(speaker)
+    )
+    toilet_id = asyncio.create_task(
+        service.register_device(toilet)
+    )
+
+    await asyncio.gather(hue_light_id, speaker_id, toilet_id)
     # create a few programs
+    wake_up_program_parallel = [
+        Message(hue_light_id.result(), MessageType.SWITCH_ON),
+        Message(speaker_id.result(), MessageType.SWITCH_ON),
+    ]
     wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(speaker_id.result(), MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
     ]
 
+    sleep_program_parallel = [
+        Message(hue_light_id.result(), MessageType.SWITCH_OFF),
+        Message(speaker_id.result(), MessageType.SWITCH_OFF),
+    ]
     sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
+        Message(toilet_id.result(), MessageType.FLUSH),
+        Message(toilet_id.result(), MessageType.CLEAN),
     ]
 
-    # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await asyncio.create_task(service.run_program(wake_up_program_parallel, parallel=True))
+    await asyncio.create_task(service.run_program(wake_up_program))
+
+    await asyncio.create_task(
+        service.run_program(sleep_program_parallel)
+    )
+    await asyncio.create_task(
+        service.run_program(sleep_program)
+    )
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
